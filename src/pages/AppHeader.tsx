50ef269a2d6ef79d25d44f6023a2139255ac8a31
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-03 23:57:26 +0100

    Test mobile behaviour

--- src/pages/AppHeader.tsx
+++ src/pages/AppHeader.tsx
@@ -1,5 +1,7 @@
+import { createMedia } from '@artsy/fresnel'
+import { useState } from 'react'
 import { Link, Location, useLocation } from 'react-router-dom'
-import { Container, Icon, Menu, MenuItem, SemanticICONS } from 'semantic-ui-react'
+import { Container, Icon, Menu, MenuItem, Segment, SemanticICONS, Sidebar } from 'semantic-ui-react'
 import { ABOUT_ROUTE, CONTACT_ROUTE, GALLERY_ROUTE, HOME_ROUTE, PRICES_ROUTE } from '../constants/Constants'
 
 const AppHeader = () => {
@@ -10,6 +12,14 @@ const AppHeader = () => {
     readonly icon: SemanticICONS,
     readonly route: string,
   }
+
+  const { MediaContextProvider, Media } = createMedia({
+    breakpoints: {
+      mobile: 0,
+      tablet: 768,
+      computer: 1024,
+    },
+  })
 
   const menuItems: MenuItem[] = [
     {
@@ -52,37 +62,116 @@ const AppHeader = () => {
     boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
   }
 
+  interface SideBar {
+    sidebarOpened?: boolean,
+  }
+
+  const [sidebarOpened, setSidebarOpened] = useState<SideBar>({ sidebarOpened: false })
+
+  const handleSidebarHide = () => setSidebarOpened({ sidebarOpened: false })
+
+  const handleToggle = () => setSidebarOpened({ sidebarOpened: true })
+
+  const gap = sidebarOpened.sidebarOpened ? 50 * 5 : 0
+
   return (
     <>
-      <Menu
-        borderless
-        icon='labeled'
-        fixed='top'
-        compact
-        style={fixedMenuStyle}
-      >
-        <Container text>
-          {
-            menuItems.map((item, index) =>
-              <Link to={item.route} key={index}>
-                <MenuItem
-                  name={item.id}
-                  active={location.pathname === item.route}
-                >
-                  <Icon name={item.icon} />
-                  {item.label}
-                </MenuItem>
-              </Link>
-            )
-          }
-        </Container>
-      </Menu>
+      <MediaContextProvider>
+        <Media greaterThan='mobile'>
+          <Menu
+            borderless
+            icon='labeled'
+            fixed='top'
+            compact
+            style={fixedMenuStyle}
+          >
+            <Container text>
+              {
+                menuItems.map((item, index) =>
+                  <Link to={item.route} key={index}>
+                    <MenuItem
+                      name={item.id}
+                      active={location.pathname === item.route}
+                    >
+                      <Icon name={item.icon} />
+                      {item.label}
+                    </MenuItem>
+                  </Link>
+                )
+              }
+            </Container>
+          </Menu>
+        </Media>
+
+        <Media at='mobile'>
+          <Sidebar.Pushable>
+            <Sidebar
+              as={Menu}
+              animation='overlay'
+              // inverted
+              onHide={handleSidebarHide}
+              vertical
+              visible={sidebarOpened.sidebarOpened}
+            >
+              <Container text>
+
+                {/* <Menu.Item onClick={handleToggle}>
+                  <Icon name='sidebar' />
+                </Menu.Item> */}
+
+                {
+                  menuItems.map((item, index) =>
+                    <Link to={item.route} key={index}>
+                      <MenuItem
+                        name={item.id}
+                        active={location.pathname === item.route}
+                      >
+                        <Icon name={item.icon} />
+                        {item.label}
+                      </MenuItem>
+                    </Link>
+                  )
+                }
+              </Container>
+            </Sidebar>
+
+            <Sidebar.Pusher>
+              <Segment
+                // inverted
+                textAlign='center'
+                style={{ minHeight: gap, padding: '1em 0em' }}
+                vertical
+              >
+                <Container>
+                  <Menu inverted pointing secondary size='large'>
+                    <Menu.Item onClick={handleToggle}>
+                      <Icon name='sidebar' />
+                    </Menu.Item>
+                    {/* <Menu.Item position='right'>
+                      <Button as='a' inverted>
+                        Log in
+                      </Button>
+                      <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                        Sign Up
+                      </Button>
+                    </Menu.Item> */}
+                  </Menu>
+                </Container>
+                {/* <HomepageHeading mobile /> */}
+              </Segment>
+
+              {/* {children} */}
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </Media>
+      </MediaContextProvider>
+
 
       {/*
       The header's menu bar will normally overlay the selected page's content, which hides the top of the page.
       To avoid this we create a fixed height container in our header to push the page content down.
       */}
-      <Container text style={{ height: '100px' }} />
+      {/* <Container text style={{ height: '100px' }} /> */}
     </>
   )
 }
