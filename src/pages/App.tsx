375081abcc6b4bba9a712618807201a6a251cb23
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-06 13:50:33 +0100

    Initial version of home page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,3 +1,4 @@
+import { createMedia } from '@artsy/fresnel'
 import { Outlet } from 'react-router-dom'
 import { Container, Header } from 'semantic-ui-react'
 import './App.css'
@@ -12,30 +13,57 @@ const App = () => {
     padding: '3em 0em',
   }
 
+  const hugeText = {
+    fontSize: '4em',
+    fontWeight: 'normal',
+    marginBottom: '0.5em',
+    marginTop: '1em',
+  }
+
+  const largeText = {
+    fontSize: '3em',
+    fontWeight: 'normal',
+    marginBottom: '1em',
+    marginTop: '0.5em',
+  }
+
+  const name = 'MELANIE NICHOLAS'
+  const occupation = 'Animal Portrait Artist'
+
+  const { MediaContextProvider, Media } = createMedia({
+    breakpoints: {
+      mobile: 0,
+      tablet: 768,
+      computer: 1024,
+    },
+  })
+
   return (
     <>
       <div className="background-image">
-        <AppHeader />
-        <Header size='huge' textAlign='center' inverted style={{
-          fontSize: '4em',
-          fontWeight: 'normal',
-          marginBottom: '0.5em',
-          marginTop: '1em',
-        }}>
-          MELANIE NICHOLAS
-        </Header>
-        <Header size='large' textAlign='center' inverted style={{
-          fontSize: '3em',
-          fontWeight: 'normal',
-          marginBottom: '1em',
-          marginTop: '0.5em',
-        }}>
-          Animal Portrait Artist
-        </Header>
-        <Container style={contentStyle}>
-          <Outlet />
-        </Container>
-        <AppFooter />
+        <MediaContextProvider>
+          <AppHeader />
+          <Media greaterThan='mobile'>
+            <Header size='huge' textAlign='center' inverted style={hugeText}>
+              {name}
+            </Header>
+            <Header size='large' textAlign='center' inverted style={largeText}>
+              {occupation}
+            </Header>
+          </Media>
+          <Media at='mobile'>
+            <Header size='huge' textAlign='center' inverted>
+              {name}
+            </Header>
+            <Header size='large' textAlign='center' inverted>
+              {occupation}
+            </Header>
+          </Media>
+          <Container style={contentStyle}>
+            <Outlet />
+          </Container>
+          <AppFooter />
+        </MediaContextProvider>
       </div>
     </>
   )
