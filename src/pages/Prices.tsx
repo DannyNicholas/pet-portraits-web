3f55cfc169f75a906723529df90f2d8d9167182b
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-07 18:16:40 +0100

    Update Prices page

--- src/pages/Prices.tsx
+++ src/pages/Prices.tsx
@@ -1,25 +1,110 @@
-import { Container, Divider, Header } from "semantic-ui-react"
+import { Container, Divider, Header, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from "semantic-ui-react"
 
-const Prices = () =>
-(
-    <Container text>
-        <Header as='h2'>Prices and Commissioning</Header>
-        <Divider />
-        <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-            ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-            magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-            quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-            arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-            Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-            dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-            Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-            viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-            Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-    </Container>
-)
+const Prices = () => {
+
+    type TableItem = {
+        readonly size: string,
+        readonly unmouted: string,
+        readonly mounted: string,
+        readonly postage: string,
+    }
+
+    const tableItems: TableItem[] = [
+        {
+            size: '6" x 6"',
+            unmouted: '10" x 10" £150',
+            mounted: '10" x 10" £160',
+            postage: '£12',
+        },
+        {
+            size: '9" x 6"',
+            unmouted: '12" x 10" £180',
+            mounted: '14" x 11" £190',
+            postage: '£12',
+        },
+        {
+            size: '10" x 7"',
+            unmouted: '14" x 11" £220',
+            mounted: '16" x 12" £230',
+            postage: '£12',
+        },
+        {
+            size: '12" x 8"',
+            unmouted: '16" x 12" £260',
+            mounted: '20" x 16" £270',
+            postage: '£12',
+        },
+    ]
+
+    return (
+        <Container text>
+            <Header as='h2'>Prices and Commissioning</Header>
+            <Divider />
+            <p>
+                I work from photos of your beloved pet, so the photo is key to the quality of the drawing.
+                The better the photo, the better the portrait will be.
+                Ideally I work from one photo but I can use a selection of photos if you do not have one clear one.
+                See Mollie and Maud as examples of where I have used more than one photo to complete a drawing.
+            </p>
+            <Header as='h3'>How to take the best photograph</Header>
+            <ul>
+                <li>
+                    Take the photo outside or in natural light.
+                    Avoid using the flash or taking a photo on a very bright day.
+                    Taking your photo on a cloudy day or near a window is ideal.
+                </li>
+                <li>
+                    Get down to the same level as your pet.
+                </li>
+                <li>
+                    Take a close up, well focussed photo.
+                </li>
+                <li>
+                    Don't reduce the size of the image when emailing.
+                </li>
+                <li>
+                    If you don't have any digital photos to email, please send photos by registered delivery,
+                    special delivery or signed for delivery. I will return them with your drawing.
+                </li>
+            </ul>
+            <p>
+                Each drawing takes between 2 and 4 weeks.
+                Please let me know the date for which you would like the drawing completed
+                in your initial contact with me.
+                It is always best to give me as much notice as possible,
+                particularly in the run up to Christmas.
+                Please contact me if you have any questions.
+            </p>
+            <Header as='h3'>Prices</Header>
+            <p>
+                Here is a list of the sizes and prices of my drawings.
+                If you would like a different size than listed below
+                I am happy to oblige so please let me know.
+            </p>
+            <Table striped>
+                <TableHeader>
+                    <TableRow textAlign='center'>
+                        <TableHeaderCell>SIZE OF DRAWING</TableHeaderCell>
+                        <TableHeaderCell>UNMOUNTED SIZE AND PRICE</TableHeaderCell>
+                        <TableHeaderCell>MOUNTED SIZE AND PRICE</TableHeaderCell>
+                        <TableHeaderCell>POSTAGE AND PACKAGING COSTS</TableHeaderCell>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        tableItems.map((row, index) =>
+                            <TableRow key={index} textAlign='center'>
+                                <TableCell>{row.size}</TableCell>
+                                <TableCell>{row.unmouted}</TableCell>
+                                <TableCell>{row.mounted}</TableCell>
+                                <TableCell>{row.postage}</TableCell>
+                            </TableRow>
+                        )
+                    }
+                </TableBody>
+            </Table>
+        </Container>
+    )
+}
 
 export default Prices
