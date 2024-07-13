2c812a8c58e37aa0f65e431c7be84a26ec42872b
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-13 16:28:41 +0100

    Fix typo

--- src/pages/ExampleSummary.tsx
+++ src/pages/ExampleSummary.tsx
@@ -12,7 +12,7 @@ const ExampleSummary = () => (
                 Ideally I work from one photo but sometimes I have used more than one photo to complete a drawing.
             </p>
             <p>
-                Click each pet to see the portraits and the original photos.
+                Click on each pet to see the portraits and the original photos.
             </p>
             {Examples
                 .sort((a, b) => a.header.localeCompare(b.header))
