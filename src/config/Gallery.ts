be58be80fe840f260ab14e586c3454f027f2274b
Danny Nicholas <dannynicholas23@gmail.com>
2024-07-16 08:15:08 +0100

    Create multiple resolution images

--- src/config/Gallery.ts
+++ src/config/Gallery.ts
@@ -1,177 +1,246 @@
 import { Photo } from 'react-photo-album';
-import BertieThumb from "../assets/pets/bertie/bertie-thumb.jpg";
-import Bertie from "../assets/pets/bertie/bertie.jpg";
-import Fili from "../assets/pets/fili/fili.jpg";
-import George from "../assets/pets/george/george.jpg";
-import Jazz from "../assets/pets/jazz/jazz.jpg";
-import Legend from "../assets/pets/legend/legend.jpg";
-import Maud from "../assets/pets/maud/maud.jpg";
-import MerlinOrig from "../assets/pets/merlin/merlin - original.jpg";
+import Bertie1024 from "../assets/pets/bertie/bertie-1024.jpg";
+import Bertie1920 from "../assets/pets/bertie/bertie-1920.jpg";
+import Bertie256 from "../assets/pets/bertie/bertie-256.jpg";
+import Bertie512 from "../assets/pets/bertie/bertie-512.jpg";
+import Fili1024 from "../assets/pets/fili/fili-1024.jpg";
+import Fili1920 from "../assets/pets/fili/fili-1920.jpg";
+import Fili256 from "../assets/pets/fili/fili-256.jpg";
+import Fili512 from "../assets/pets/fili/fili-512.jpg";
+import George1024 from "../assets/pets/george/george-1024.jpg";
+import George1920 from "../assets/pets/george/george-1920.jpg";
+import George256 from "../assets/pets/george/george-256.jpg";
+import George512 from "../assets/pets/george/george-512.jpg";
+import Jazz1024 from "../assets/pets/jazz/jazz-1024.jpg";
+import Jazz1920 from "../assets/pets/jazz/jazz-1920.jpg";
+import Jazz256 from "../assets/pets/jazz/jazz-256.jpg";
+import Jazz512 from "../assets/pets/jazz/jazz-512.jpg";
+import Legend1024 from "../assets/pets/legend/legend-1024.jpg";
+import Legend1920 from "../assets/pets/legend/legend-1920.jpg";
+import Legend256 from "../assets/pets/legend/legend-256.jpg";
+import Legend512 from "../assets/pets/legend/legend-512.jpg";
+import Maud1024 from "../assets/pets/maud/maud-1024.jpg";
+import Maud1920 from "../assets/pets/maud/maud-1920.jpg";
+import Maud256 from "../assets/pets/maud/maud-256.jpg";
+import Maud512 from "../assets/pets/maud/maud-512.jpg";
+import Merlin1024 from "../assets/pets/merlin/merlin-1024.jpg";
+import Merlin1920 from "../assets/pets/merlin/merlin-1920.jpg";
 import Merlin256 from "../assets/pets/merlin/merlin-256.jpg";
 import Merlin512 from "../assets/pets/merlin/merlin-512.jpg";
-import Merlin from "../assets/pets/merlin/merlin.jpg";
-import Mitzy from "../assets/pets/mitzy/mitzy.jpg";
-import Mollie from "../assets/pets/mollie/mollie.jpg";
-import Mustard from "../assets/pets/mustard/mustard.jpg";
-import Oscar from "../assets/pets/oscar/oscar.jpg";
-import Saffy from "../assets/pets/saffy/saffy.jpg";
-import Soldus from "../assets/pets/soldus/soldus.jpg";
-import Sonny from "../assets/pets/sonny/sonny.jpg";
-import Tommy from "../assets/pets/tommy/tommy.jpg";
+import Mitzy1024 from "../assets/pets/mitzy/mitzy-1024.jpg";
+import Mitzy1920 from "../assets/pets/mitzy/mitzy-1920.jpg";
+import Mitzy256 from "../assets/pets/mitzy/mitzy-256.jpg";
+import Mitzy512 from "../assets/pets/mitzy/mitzy-512.jpg";
+import Mollie1024 from "../assets/pets/mollie/mollie-1024.jpg";
+import Mollie1920 from "../assets/pets/mollie/mollie-1920.jpg";
+import Mollie256 from "../assets/pets/mollie/mollie-256.jpg";
+import Mollie512 from "../assets/pets/mollie/mollie-512.jpg";
+import Mustard1024 from "../assets/pets/mustard/mustard-1024.jpg";
+import Mustard1920 from "../assets/pets/mustard/mustard-1920.jpg";
+import Mustard256 from "../assets/pets/mustard/mustard-256.jpg";
+import Mustard512 from "../assets/pets/mustard/mustard-512.jpg";
+import Oscar1024 from "../assets/pets/oscar/oscar-1024.jpg";
+import Oscar1920 from "../assets/pets/oscar/oscar-1920.jpg";
+import Oscar256 from "../assets/pets/oscar/oscar-256.jpg";
+import Oscar512 from "../assets/pets/oscar/oscar-512.jpg";
+import Saffy1024 from "../assets/pets/saffy/saffy-1024.jpg";
+import Saffy1920 from "../assets/pets/saffy/saffy-1920.jpg";
+import Saffy256 from "../assets/pets/saffy/saffy-256.jpg";
+import Saffy512 from "../assets/pets/saffy/saffy-512.jpg";
+import Soldus1024 from "../assets/pets/soldus/soldus-1024.jpg";
+import Soldus1920 from "../assets/pets/soldus/soldus-1920.jpg";
+import Soldus256 from "../assets/pets/soldus/soldus-256.jpg";
+import Soldus512 from "../assets/pets/soldus/soldus-512.jpg";
+import Sonny1024 from "../assets/pets/sonny/sonny-1024.jpg";
+import Sonny1920 from "../assets/pets/sonny/sonny-1920.jpg";
+import Sonny256 from "../assets/pets/sonny/sonny-256.jpg";
+import Sonny512 from "../assets/pets/sonny/sonny-512.jpg";
+import Tommy1024 from "../assets/pets/tommy/tommy-1024.jpg";
+import Tommy1920 from "../assets/pets/tommy/tommy-1920.jpg";
+import Tommy256 from "../assets/pets/tommy/tommy-256.jpg";
+import Tommy512 from "../assets/pets/tommy/tommy-512.jpg";
 
 
 // holds photos for the gallery
 export const Photos: Photo[] = [
     {
-        src: Oscar,
-        width: 1000,
-        height: 1000,
+        src: Oscar1920,
+        width: 1920,
+        height: 1797,
         title: "Oscar",
         srcSet: [
-            { src: Oscar, width: 1000, height: 1000 },
-            { src: Oscar, width: 1000, height: 1000 },
+            { src: Oscar256, width: 256, height: 240 },
+            { src: Oscar512, width: 512, height: 479 },
+            { src: Oscar1024, width: 1024, height: 958 },
+            { src: Oscar1920, width: 1920, height: 1797 },
         ]
     },
     {
-        src: Maud,
-        width: 1000,
-        height: 1000,
+        src: Maud1920,
+        width: 1920,
+        height: 1639,
         title: "Maud",
         srcSet: [
-            { src: Maud, width: 1000, height: 1000 },
-            { src: Maud, width: 1000, height: 1000 },
+            { src: Maud256, width: 256, height: 219 },
+            { src: Maud512, width: 512, height: 437 },
+            { src: Maud1024, width: 1024, height: 874 },
+            { src: Maud1920, width: 1920, height: 1639 },
         ]
     },
     {
-        src: George,
-        width: 1000,
-        height: 1000,
+        src: George1920,
+        width: 1920,
+        height: 1548,
         title: "George",
         srcSet: [
-            { src: George, width: 1000, height: 1000 },
-            { src: George, width: 1000, height: 1000 },
+            { src: George256, width: 256, height: 206 },
+            { src: George512, width: 512, height: 413 },
+            { src: George1024, width: 1024, height: 826 },
+            { src: George1920, width: 1920, height: 1548 },
         ]
     },
     {
-        src: Merlin,
-        width: 3423,
-        height: 2419,
+        src: Merlin1920,
+        width: 1920,
+        height: 1357,
         title: "Merlin",
         srcSet: [
             { src: Merlin256, width: 256, height: 181 },
             { src: Merlin512, width: 512, height: 362 },
-            { src: Merlin, width: 1280, height: 905 },
-            { src: MerlinOrig, width: 3423, height: 2419 },
+            { src: Merlin1024, width: 1024, height: 724 },
+            { src: Merlin1920, width: 1920, height: 1357 },
         ]
     },
     {
-        src: Bertie,
-        width: 5428,
-        height: 4046,
+        src: Bertie1920,
+        width: 1920,
+        height: 1431,
         title: "Bertie",
         srcSet: [
-            { src: BertieThumb, width: 150, height: 150 },
-            { src: Bertie, width: 5428, height: 4046 },
+            { src: Bertie256, width: 256, height: 191 },
+            { src: Bertie512, width: 512, height: 382 },
+            { src: Bertie1024, width: 1024, height: 763 },
+            { src: Bertie1920, width: 1920, height: 1431 },
         ]
     },
     {
-        src: Soldus,
-        width: 1000,
-        height: 1000,
+        src: Soldus1920,
+        width: 1920,
+        height: 1743,
         title: "Soldus",
         srcSet: [
-            { src: Soldus, width: 1000, height: 1000 },
-            { src: Soldus, width: 1000, height: 1000 },
+            { src: Soldus256, width: 256, height: 232 },
+            { src: Soldus512, width: 512, height: 465 },
+            { src: Soldus1024, width: 1024, height: 930 },
+            { src: Soldus1920, width: 1920, height: 1743 },
         ]
     },
     {
-        src: Sonny,
-        width: 1000,
-        height: 1000,
+        src: Sonny1920,
+        width: 1920,
+        height: 1512,
         title: "Sonny",
         srcSet: [
-            { src: Sonny, width: 1000, height: 1000 },
-            { src: Sonny, width: 1000, height: 1000 },
+            { src: Sonny256, width: 256, height: 202 },
+            { src: Sonny512, width: 512, height: 403 },
+            { src: Sonny1024, width: 1024, height: 807 },
+            { src: Sonny1920, width: 1920, height: 1512 },
         ]
     },
     {
-        src: Mollie,
-        width: 1000,
-        height: 1000,
+        src: Mollie1920,
+        width: 1920,
+        height: 1440,
         title: "Mollie",
         srcSet: [
-            { src: Mollie, width: 1000, height: 1000 },
-            { src: Mollie, width: 1000, height: 1000 },
+            { src: Mollie256, width: 256, height: 192 },
+            { src: Mollie512, width: 512, height: 384 },
+            { src: Mollie1024, width: 1024, height: 768 },
+            { src: Mollie1920, width: 1920, height: 1440 },
         ]
     },
     {
-        src: Tommy,
-        width: 1000,
-        height: 1000,
-        title: "Sonny",
+        src: Tommy1920,
+        width: 1920,
+        height: 2880,
+        title: "Tommy",
         srcSet: [
-            { src: Tommy, width: 1000, height: 1000 },
-            { src: Tommy, width: 1000, height: 1000 },
+            { src: Tommy256, width: 256, height: 384 },
+            { src: Tommy512, width: 512, height: 768 },
+            { src: Tommy1024, width: 1024, height: 1536 },
+            { src: Tommy1920, width: 1920, height: 2880 },
         ]
     },
     {
-        src: Mitzy,
-        width: 3633,
-        height: 3548,
+        src: Mitzy1920,
+        width: 1920,
+        height: 1875,
         title: "Mitzy",
         srcSet: [
-            { src: Mitzy, width: 3633, height: 3548 },
-            { src: Mitzy, width: 1000, height: 1000 },
+            { src: Mitzy256, width: 256, height: 250 },
+            { src: Mitzy512, width: 512, height: 500 },
+            { src: Mitzy1024, width: 1024, height: 1000 },
+            { src: Mitzy1920, width: 1920, height: 1875 },
         ]
     },
     {
-        src: Mustard,
-        width: 1000,
-        height: 1000,
+        src: Mustard1920,
+        width: 1920,
+        height: 1554,
         title: "Mustard",
         srcSet: [
-            { src: Mustard, width: 1000, height: 1000 },
-            { src: Mustard, width: 1000, height: 1000 },
+            { src: Mustard256, width: 256, height: 207 },
+            { src: Mustard512, width: 512, height: 414 },
+            { src: Mustard1024, width: 1024, height: 829 },
+            { src: Mustard1920, width: 1920, height: 1554 },
         ]
     },
     {
-        src: Jazz,
-        width: 1000,
-        height: 1000,
+        src: Jazz1920,
+        width: 1920,
+        height: 1730,
         title: "Jazz",
         srcSet: [
-            { src: Jazz, width: 1000, height: 1000 },
-            { src: Jazz, width: 1000, height: 1000 },
+            { src: Jazz256, width: 256, height: 231 },
+            { src: Jazz512, width: 512, height: 461 },
+            { src: Jazz1024, width: 1024, height: 923 },
+            { src: Jazz1920, width: 1920, height: 1730 },
         ]
     },
     {
-        src: Saffy,
-        width: 1000,
-        height: 1000,
+        src: Saffy1920,
+        width: 1920,
+        height: 2366,
         title: "Saffy",
         srcSet: [
-            { src: Saffy, width: 1000, height: 1000 },
-            { src: Saffy, width: 1000, height: 1000 },
+            { src: Saffy256, width: 256, height: 315 },
+            { src: Saffy512, width: 512, height: 631 },
+            { src: Saffy1024, width: 1024, height: 1262 },
+            { src: Saffy1920, width: 1920, height: 2366 },
         ]
     },
     {
-        src: Fili,
-        width: 1000,
-        height: 1000,
+        src: Fili1920,
+        width: 1920,
+        height: 2035,
         title: "Fili",
         srcSet: [
-            { src: Fili, width: 1000, height: 1000 },
-            { src: Fili, width: 1000, height: 1000 },
+            { src: Fili256, width: 256, height: 271 },
+            { src: Fili512, width: 512, height: 543 },
+            { src: Fili1024, width: 1024, height: 1086 },
+            { src: Fili1920, width: 1920, height: 2035 },
         ]
     },
     {
-        src: Legend,
-        width: 1000,
-        height: 1000,
+        src: Legend1920,
+        width: 1920,
+        height: 2611,
         title: "Legend",
         srcSet: [
-            { src: Legend, width: 1000, height: 1000 },
-            { src: Legend, width: 1000, height: 1000 },
+            { src: Legend256, width: 256, height: 348 },
+            { src: Legend512, width: 512, height: 696 },
+            { src: Legend1024, width: 1024, height: 1393 },
+            { src: Legend1920, width: 1920, height: 2611 },
         ]
     },
 ];
